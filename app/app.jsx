9a06c0f28f5fd18716c9d828a009a716bb4826a1
waddmat <matthew.waddingham@nhs.net>
2016-09-13 16:02:46 +0100

    switch to scss

--- app/app.jsx
+++ app/app.jsx
@@ -6,7 +6,7 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Router history={hashHistory}>
