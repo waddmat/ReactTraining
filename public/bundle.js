b06887b2f494894fc5cac763752aa31581105dbf
waddmat <matthew.waddingham@nhs.net>
2016-09-28 15:14:51 +0100

    mocha chai testing

--- public/bundle.js
+++ public/bundle.js
@@ -46,26 +46,13 @@
 
 	'use strict';
 
-	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(158);
-
-	var objOne = {
-	  name: 'matt',
-	  location: 'grimsby'
-	};
-
-	var objTwo = _extends({
-	  age: 35
-	}, objOne);
-
-	console.log(objTwo);
 
 	ReactDOM.render(React.createElement(
 	  'h1',
 	  null,
-	  'Boilerplate'
+	  'ReduxStack'
 	), document.getElementById('app'));
 
 /***/ },
